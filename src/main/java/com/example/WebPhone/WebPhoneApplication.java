140a903e0ed36a8fe243c5b9a2b5f5d026d597ac
voxquoocshuyy <66914322+voxquoocshuyy@users.noreply.github.com>
2021-10-19 09:42:48 +0700

    First commit

---
+++ src/main/java/com/example/WebPhone/WebPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebPhone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPhoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebPhone/WebPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebPhone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
