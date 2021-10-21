16c9fd7db6c3c78d4a32353a4339f4a86cca6714
voxquoocshuyy <66914322+voxquoocshuyy@users.noreply.github.com>
2021-10-21 11:00:50 +0700

    add product complete

--- src/main/java/com/example/WebPhone/repository/IProductRepository.java
+++ src/main/java/com/example/WebPhone/repository/IProductRepository.java
@@ -3,6 +3,6 @@ package com.example.WebPhone.repository;
 import com.example.WebPhone.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IProductRepository extends JpaRepository<Product, String> {
+public interface IProductRepository extends JpaRepository<Product, Integer> {
 
 }
